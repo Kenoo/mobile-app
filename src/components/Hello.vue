4b25cb5de3877763932a514e422b793ed3210aa4
kenoo <312008981@qq.com>
2017-08-09 00:34:45 +0800

    add vux component

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,44 +1,25 @@
 <template>
-  <div class="hello">
-    <h1 @click="goto('a')">{{ msg }}</h1>
-
+  <div>
+    <group>
+      <cell title="title" value="1">
+        <span @click="goto('a')">2</span>
+      </cell>
+    </group>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  },
-  methods: {
-    goto (page) {
-      this.$router.push(page)
-    }
+  import { Group, Cell } from 'vux'
 
+  export default {
+    components: {
+      Group,
+      Cell
+    },
+    methods: {
+      goto (page) {
+        this.$router.push(page)
+      }
+    }
   }
-}
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
